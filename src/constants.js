b571f050414f1572aa521a05047a54d812ceb50f
Sushincm <sushinofficial@gmail.com>
2026-05-06 15:42:57 +0530

    Add NZDUSD to currency pairs

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,8 @@
-// Core constants and configurations
+  // Core constants and configurations
 
 export const PAIRS = [
   'AUDJPY', 'EURGBP', 'GBPUSD', 'CADCHF', 'EURUSD', 'GBPJPY', 
-  'USDCHF', 'USDJPY', 'CADJPY', 'AUDUSD', 'USDCAD', 'GOLDUSD'
+  'USDCHF', 'USDJPY', 'CADJPY', 'AUDUSD', 'USDCAD', 'GOLDUSD', 'NZDUSD'
 ];
 
 export const PIP_VALUES = {
@@ -10,6 +10,7 @@ export const PIP_VALUES = {
   EURUSD: 10,
   GBPUSD: 10,
   AUDUSD: 10,
+  NZDUSD: 10,
   GOLDUSD: 10, // $10 per pip (actually 300 points = 30 pips, so 30 * 10 = $300 risk per standard lot)
   
   // Non-USD/Cross pairs (estimated based on average conversion rates)
